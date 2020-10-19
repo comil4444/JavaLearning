7eeebd57de7b4e1e1c50b19108e5bcd8806da7bc
cuishijie <cuishijie1@jd.com>
2020-10-19 10:11:40 +0800

    add comments

--- src/main/java/com/cn/sort/InsertSort.java
+++ src/main/java/com/cn/sort/InsertSort.java
@@ -5,14 +5,18 @@ public class InsertSort implements ISort{
     public void sort(int[] arr) {
         for (int i = 1; i < arr.length; i++)
         {
-            int j;
+            //从小到大排序，如果当前元素已经大于前序有序元素最大的，则直接跳过
             if (arr[i] < arr[i - 1])
             {
                 int temp = arr[i];
+                int j;
+                //从后往前比较
                 for (j = i - 1; j >= 0 && temp < arr[j]; j--)
                 {
+                    //挪动元素，为了插入排序腾位置
                     arr[j + 1] = arr[j];
                 }
+                //腾出的位置直接插入
                 arr[j + 1] = temp;
             }
         }
